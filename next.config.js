96303b71bd708f6cc7861e2a82678f2f53b3801e
subrahmanya G T <subrahmanyagt@gmail.com>
2022-05-12 17:40:46 +0530

    Rewrite

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: "/Home",
+        source: "/",
         destination: "https://cooler-power.webflow.io/",
       },
       {
